63e80bbf1603f97fa49bb6b2859c585fb2d0eea3
Armando Castillo <armando.castillo@sundevs.com>
2018-10-05 14:25:44 -0500

    Fix inexistent file import

--- splash-page/form_processing.php
+++ splash-page/form_processing.php
@@ -1,7 +1,6 @@
 <?php
 require_once(dirname(__FILE__).'/../class/Log.php');
 require_once(dirname(__FILE__).'/../class/Utils.php');
-require_once(dirname(__FILE__).'/../class/SimpleAWS.php');
 require_once(dirname(__FILE__).'/../constants.php');
 
 $token = $_POST['token'];
